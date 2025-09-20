23140ead52113070d0384bf0b81b32dd46870870
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-09-20 09:37:53 +0200

    Initial commit: Add DicePlugin project files
    Add all source files, configuration, resources, and packaging metadata for the DicePlugin Loupedeck plugin. Includes dice action classes (D4, D6, D8, D10, D12, D20), plugin infrastructure, helper utilities, images, VS Code settings, and build scripts.

---
+++ src/Actions/D4.cs
@@ -0,0 +1,48 @@
+﻿namespace Loupedeck.DicePlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class D4 : PluginDynamicCommand
+    {
+        //private readonly String _imageResourcePath1;
+
+        private Random random = new Random();
+        private int hand;
+        private const int value = 4;
+
+        // Initializes the command class.
+        public D4()
+            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
+        {
+            //this._imageResourcePath1 = PluginResources.FindFile("Images/D4/1.png");
+        }
+
+        // This method is called when the user executes the command.
+        protected override void RunCommand(String actionParameter)
+        {
+            this.hand = random.Next(value) + 1;
+            this.ActionImageChanged(); // Notify the plugin service that the command display name and/or image has changed.
+            PluginLog.Info($"Throw value is {this.hand}"); // Write the current counter value to the log file.
+        }
+
+        // This method is called when Loupedeck needs to show the command on the console or the UI.
+        protected override String GetCommandDisplayName(String actionParameter, PluginImageSize imageSize) =>
+            $"";
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                bitmapBuilder.SetBackgroundImage(PluginResources.ReadImage("D4.png"));
+                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}");
+
+                return bitmapBuilder.ToImage();
+            }
+        }
+
+    }
+}
---
+++ src/Actions/D6.cs
@@ -0,0 +1,33 @@
+﻿namespace Loupedeck.DicePlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class D6 : PluginDynamicCommand
+    {
+        private Random random = new Random();
+        private int hand;
+        private const int value = 6;
+
+        // Initializes the command class.
+        public D6()
+            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
+        {
+        }
+
+        // This method is called when the user executes the command.
+        protected override void RunCommand(String actionParameter)
+        {
+            this.hand = random.Next(value) + 1;
+            this.ActionImageChanged(); // Notify the plugin service that the command display name and/or image has changed.
+            PluginLog.Info($"Throw value is {this.hand}"); // Write the current counter value to the log file.
+        }
+
+        // This method is called when Loupedeck needs to show the command on the console or the UI.
+        protected override String GetCommandDisplayName(String actionParameter, PluginImageSize imageSize) =>
+            $"D{value}{Environment.NewLine}{this.hand}";
+    }
+}
