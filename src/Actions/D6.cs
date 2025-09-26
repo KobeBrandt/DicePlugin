446f38db3327d7976a3c0236b4f3637d67c4aa3b
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-09-26 15:00:03 +0200

    All 6 done

--- src/Actions/D6.cs
+++ src/Actions/D6.cs
@@ -16,6 +16,7 @@
         public D6()
             : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
         {
+            base.IsWidget = true;
         }
 
         // This method is called when the user executes the command.
@@ -27,7 +28,21 @@
         }
 
         // This method is called when Loupedeck needs to show the command on the console or the UI.
-        protected override String GetCommandDisplayName(String actionParameter, PluginImageSize imageSize) =>
-            $"D{value}{Environment.NewLine}{this.hand}";
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                //bitmapBuilder.DrawText();
+                var lineHeight = 32;
+                var fontSize = 32;
+                // Shadow (2px Offset)
+                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}", fontSize: fontSize, lineHeight: lineHeight);
+
+
+
+
+                return bitmapBuilder.ToImage();
+            }
+        }
     }
 }
