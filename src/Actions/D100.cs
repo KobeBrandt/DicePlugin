32662e7e209fcb0d233d2067ac7620b9c7cf3746
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-09-26 15:43:32 +0200

    added D100 and D10000

---
+++ src/Actions/D100.cs
@@ -0,0 +1,43 @@
+﻿namespace Loupedeck.DicePlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class D100 : PluginDynamicCommand
+    {
+        private Random random = new Random();
+        private int hand;
+        private const int value = 100;
+
+        public D100()
+            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
+        {
+            base.IsWidget = true;
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            this.hand = random.Next(value) + 1;
+            this.ActionImageChanged();
+            PluginLog.Info($"Throw value is {this.hand}");
+        }
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                var lineHeight = 32;
+                var fontSize = 32;
+                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}", fontSize: fontSize, lineHeight: lineHeight);
+
+
+
+
+                return bitmapBuilder.ToImage();
+            }
+        }
+    }
+}
---
+++ src/Actions/D10000.cs
@@ -0,0 +1,43 @@
+﻿namespace Loupedeck.DicePlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class D10000 : PluginDynamicCommand
+    {
+        private Random random = new Random();
+        private int hand;
+        private const int value = 10000;
+
+        public D10000()
+            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
+        {
+            base.IsWidget = true;
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            this.hand = random.Next(value) + 1;
+            this.ActionImageChanged();
+            PluginLog.Info($"Throw value is {this.hand}");
+        }
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                var lineHeight = 32;
+                var fontSize = 32;
+                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}", fontSize: fontSize, lineHeight: lineHeight);
+
+
+
+
+                return bitmapBuilder.ToImage();
+            }
+        }
+    }
+}
