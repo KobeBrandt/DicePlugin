77ab25379092414ebc6cd4f43102a1b8249dd009
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-09-26 16:40:12 +0200

    Add D2 dice roll action
    Introduces a new PluginDynamicCommand 'D2' that simulates rolling a 2-sided die. The action updates its widget image with the result and logs the rolled value.

---
+++ src/Actions/D2.cs
@@ -0,0 +1,45 @@
+﻿namespace Loupedeck.DicePlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class D2 : PluginDynamicCommand
+    {
+
+
+        private Random random = new Random();
+        private int hand;
+        private const int value = 2;
+
+        public D2()
+            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
+        {
+            base.IsWidget = true;
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            this.hand = random.Next(value) + 1;
+            this.ActionImageChanged();
+            PluginLog.Info($"Throw value is {this.hand}");
+        }
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                var lineHeight = 32;
+                var fontSize = 32;
+                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}", fontSize: fontSize, lineHeight: lineHeight);
+
+
+
+
+                return bitmapBuilder.ToImage();
+            }
+        }
+    }
+}
