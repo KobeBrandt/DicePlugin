af8549cc1cf813667437512c1890b23caef32ae8
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-10-01 09:54:30 +0200

    Parent class
    Implemented a parent class

--- src/Actions/D10.cs
+++ src/Actions/D10.cs
@@ -2,54 +2,8 @@
 {
     using System;
 
-    internal class D10 : PluginDynamicCommand
+    internal class D10 : Dxxx
     {
-        private Random random = new Random();
-        private int hand;
-        private const int value = 10;
-
-
-        private int lineHeight = 32;
-        private int fontSize = 32;
-
-        public D10()
-            : base(displayName: $"D{value}", description: $"Rolls a {value}", groupName: "Dice")
-        {
-            base.IsWidget = true;
-        }
-
-        protected override void RunCommand(String actionParameter)
-        {
-            rollInProgress();
-            this.hand = random.Next(value) + 1;
-            rollDone();
-            PluginLog.Info($"Throw value is {this.hand}");
-        }
-
-        private void rollInProgress()
-        {
-            this.lineHeight = 16;
-            this.fontSize = 16;
-            this.ActionImageChanged();
-            Thread.Sleep(250);
-        }
-        private void rollDone()
-        {
-            this.lineHeight = 32;
-            this.fontSize = 32;
-            this.ActionImageChanged();
-        }
-
-
-        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
-        {
-            using (var bitmapBuilder = new BitmapBuilder(imageSize))
-            {
-
-                bitmapBuilder.DrawText($"D{value}{Environment.NewLine}{this.hand}", fontSize: this.fontSize, lineHeight: this.lineHeight);
-
-                return bitmapBuilder.ToImage();
-            }
-        }
+        public D10() : base(10) { }
     }
 }
